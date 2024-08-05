c490083e74e27889150a56f6321770d3624456aa
Davyd001dev01 <davyd7dev01@gmail.com>
2024-08-05 01:14:04 -0300

    chore: add shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image"
+import { Button } from "./_components/ui/button"
 
 export default function Home() {
-  return <h1 className="border-l bg-slate-500 text-2xl">Hello World!!!</h1>
+  return (
+    <div>
+      <h1 className="border-l bg-slate-500 text-2xl">Hello World!!!</h1>
+      <Button>Teste</Button>
+    </div>
+  )
 }
