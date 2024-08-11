574990c7cda424ef87a67cec18ce21ef9a52e006
Davyd001dev01 <davyd7dev01@gmail.com>
2024-08-11 01:02:28 -0300

    feat: ensure booking can be created

--- src/app/barbershops/[id]/page.tsx
+++ src/app/barbershops/[id]/page.tsx
@@ -91,7 +91,11 @@ export default async function BarberShopPage({ params }: BarbershopProps) {
         <h2 className="text-[.9rem] font-bold text-gray-400">Serviços</h2>
         <div className="space-y-3">
           {barbershop.services.map((service) => (
-            <ServiceItem key={service.id} service={service} />
+            <ServiceItem
+              key={service.id}
+              barbershop={barbershop}
+              service={service}
+            />
           ))}
         </div>
       </div>
